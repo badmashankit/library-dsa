175939d2689296f493017851e3396b77b7566508
badmashankit <ankits.2711@gmail.com>
2017-06-28 13:29:52 +0530

    Checking in Matrix Transpose code in miscellaneous

---
+++ DSA Library/src/com/badmashankit/library/misc/MatrixTranspose.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2017 Ankit Shukla, http://badmashankit.com
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.badmashankit.library.misc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MatrixTranspose {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Input no. of rows(M): ");
+		int M = Integer.parseInt(br.readLine());
+		
+		System.out.print("Input no. of columns(N): ");
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] matrix = new int[M][N];
+		
+        System.out.println("\nInput matrix of size " + M + " x " + N + " :");
+        
+        for(int i = 0; i < M; i++) {
+        	String[] input = br.readLine().split(" ");
+        	for(int j = 0; j < N; j++) {
+        		matrix[i][j] = Integer.parseInt(input[j]);
+        	}
+        }
+        
+        System.out.println("\nMatrix transposed: ");
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                System.out.print(matrix[j][i] + "\t");
+            }
+            System.out.println();
+        }
+	}
+}
