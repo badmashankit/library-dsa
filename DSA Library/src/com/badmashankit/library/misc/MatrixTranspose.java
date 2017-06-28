da4ff96c8316019ff92d38ef3f93decf34d3b62a
badmashankit <ankits.2711@gmail.com>
2017-06-28 16:28:05 +0530

    fixing indentation

--- DSA Library/src/com/badmashankit/library/misc/MatrixTranspose.java
+++ DSA Library/src/com/badmashankit/library/misc/MatrixTranspose.java
@@ -22,34 +22,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MatrixTranspose {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		System.out.print("Input no. of rows(M): ");
 		int M = Integer.parseInt(br.readLine());
-		
+
 		System.out.print("Input no. of columns(N): ");
 		int N = Integer.parseInt(br.readLine());
-		
+
 		int[][] matrix = new int[M][N];
-		
-        System.out.println("\nInput matrix of size " + M + " x " + N + " :");
-        
-        for(int i = 0; i < M; i++) {
-        	String[] input = br.readLine().split(" ");
-        	for(int j = 0; j < N; j++) {
-        		matrix[i][j] = Integer.parseInt(input[j]);
-        	}
-        }
-        
-        System.out.println("\nMatrix transposed: ");
-        
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                System.out.print(matrix[j][i] + "\t");
-            }
-            System.out.println();
-        }
+
+		System.out.println("\nInput matrix of size " + M + " x " + N + " :");
+
+		for (int i = 0; i < M; i++) {
+			String[] input = br.readLine().split(" ");
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = Integer.parseInt(input[j]);
+			}
+		}
+
+		System.out.println("\nMatrix transposed: ");
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				System.out.print(matrix[j][i] + "\t");
+			}
+			System.out.println();
+		}
 	}
 }
