1f15fc109bdbe5e90035dcfe77a629a21295c548
badmashankit <ankits.2711@gmail.com>
2017-09-11 00:58:11 +0530

    Added AllTests as TestSuite. It'll consist of all test cases

---
+++ DSA Library/test/com/badmashankit/AllTests.java
@@ -0,0 +1,15 @@
+package com.badmashankit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.badmashankit.library.StackTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	StackTest.class
+})
+public class AllTests {
+
+}
