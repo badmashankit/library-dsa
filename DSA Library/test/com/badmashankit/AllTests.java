3da2c31f435ff159c131af13884f05df277c4dd1
badmashankit <ankits.2711@gmail.com>
2017-09-12 00:19:51 +0530

    Added ArrayListTest to the AllTests Test Suite

--- DSA Library/test/com/badmashankit/AllTests.java
+++ DSA Library/test/com/badmashankit/AllTests.java
@@ -4,12 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.badmashankit.library.ArrayListTest;
 import com.badmashankit.library.StackTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({
-	StackTest.class
-})
+@SuiteClasses({ ArrayListTest.class, StackTest.class })
 public class AllTests {
 
 }
